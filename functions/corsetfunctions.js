bb815f8286e3f8d275efa94f749c7bd3d5899542
Totox00 <bladewielder.roboman@gmail.com>
2025-12-27 19:20:00 +0100

    adjust some breath values for looser corsets

--- functions/corsetfunctions.js
+++ functions/corsetfunctions.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-const MAX_BREATH_TABLE = [2000, 155, 125, 102.5, 85, 70, 60, 50, 40, 32.5, 25, 17.5, 10, 5, 5, 5];
-const BREATH_RECOVERY_TABLE = [2000, 11.5, 9.5, 8, 6.5, 5, 4, 3.2, 2.5, 2, 1.5, 1, 0.5, 0.25, 0.1, 0.02];
+const MAX_BREATH_TABLE = [2000, 140, 120, 100, 85, 70, 60, 50, 40, 32.5, 25, 17.5, 10, 5, 5, 5];
+const BREATH_RECOVERY_TABLE = [2000, 20, 15, 10, 7, 5, 4, 3.2, 2.5, 2, 1.5, 1, 0.5, 0.25, 0.1, 0.02];
 
 const gaspSounds = ["*hff*", "*hnnf*", "*ahff*", "*hhh*", "*nnn*", "*hnn*", "*hng*", "*uah*", "*uhh*"];
 const silenceReplacers = [" ", ".", ",", ""];
