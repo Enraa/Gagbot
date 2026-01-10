96f5af471d1f947c11583869552f4c4a684b499b
Pyrotech <pyratechnician@gmail.com>
2026-01-10 02:04:37 +0000

    Added Oversized Fluffy Paw Mittens

--- functions/gagfunctions.js
+++ functions/gagfunctions.js
@@ -63,6 +63,7 @@ const gagtypesout = [
 
 const mittentypes = [
     { name: "Kitty Paws", value: "mittens_kitty" },
+    { name: "Oversized Fluffy Paw Mittens", value: "mittens_oversized_fluff" },
     { name: "Cyber Doll Mittens", value: "mittens_cyberdoll" },
     { name: "Leather Mittens", value: "mittens_leather" },
     { name: "Hardlight Spheres", value: "mittens_hardlight" },
