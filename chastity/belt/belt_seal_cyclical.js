64fb7227acde7cdeebe53471aa82470714d5fc99
Enraa <enraaaery@gmail.com>
2026-04-30 23:39:51 -0700

    Adjust Cyclical to use a setInterval function within the chastity object

--- chastity/belt/belt_seal_cyclical.js
+++ chastity/belt/belt_seal_cyclical.js
@@ -1,5 +1,5 @@
 const { getUserVar, setUserVar } = require("../../functions/usercontext")
-const { clearArousal, getArousal, addArousal } = require("../../functions/vibefunctions")
+const { clearArousal, getArousal, addArousal, getChastity } = require("../../functions/vibefunctions")
 
 // Seal of Cyclical Time
 // This Seal resets the wearer to their initial state every 3 minutes
@@ -7,18 +7,39 @@ const { clearArousal, getArousal, addArousal } = require("../../functions/vibefu
 exports.denialCoefficient = (data) => { return 1 }
 
 // Events
-// Randomly reduce the level of arousal by a random percentage, then reduce by a further 10%
 exports.onEquip = (data) => {
     // Configure base arousal value
     if (!getUserVar(data.userID, "base_arousal") || getUserVar(data.userID, "base_arousal") == undefined) setUserVar(data.userID, "base_arousal", getArousal(data.userID) ?? 0);
-    if (!getUserVar(data.userID, "stasis_timer") || getUserVar(data.userID, "stasis_timer") == undefined) setUserVar(data.userID, "stasis_timer", Date.now());
+
+    // Add a timer which will reset the arousal every 3 mins
+    let chastityinterval = setInterval(() => {
+        if ((getChastity(data.userID)?.chastitytype == "belt_seal_cyclical") && getUserVar(data.userID, "base_arousal")) {
+            try {
+                clearArousal(data.userID);
+                addArousal(data.userID, getUserVar(data.userID, "base_arousal"));
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }
+        else {
+            // They're somehow not wearing the belt anymore or something else broke. 
+            try {
+                clearInterval(chastityinterval);
+                setUserVar(data.userID, "base_arousal", undefined);
+            }
+            catch (err) {
+                console.log(err)
+            }
+        }
+    }, 180000)
 }
 
 exports.onUnequip = (data) => {
     //  Add All Stored Arousal at once
+    clearArousal(data.userID);
     addArousal(data.userID, getUserVar(data.userID, "base_arousal"));
     setUserVar(data.userID, "base_arousal", undefined);
-    setUserVar(data.userID, "stasis_timer", undefined);
 }
 
 // Tags
