baeffc2fb3e45ad4e34e5d024e4711b28d81aad8
Enraa <enraaaery@gmail.com>
2026-06-08 20:52:54 -0700

    headwear now respects lockable property
    and will also only show keyed status if it is lockable

--- functions/headwearfunctions.js
+++ functions/headwearfunctions.js
@@ -109,7 +109,10 @@ const assignHeadwear = (userID, headwear, origbinder) => {
 		process.headwear[userID] = { wornheadwear: [headwear], origbinder: originalbinder ?? origbinder };
 	}
     originalbinder = ((process.headwear[userID] && process.headwear[userID][headwear] && process.headwear[userID][headwear].origbinder) ?? origbinder) ?? userID;
-    process.headwear[userID][headwear] = { origbinder: originalbinder ?? userID }
+    process.headwear[userID][headwear] = { 
+        origbinder: originalbinder ?? userID,
+        lockable: getBaseHeadwear(headwear).lockable
+    }
     // Increment the worn corset counter
     if (process.userstats == undefined) { process.userstats = {} }
     if (process.userstats[userID] == undefined) { process.userstats[userID] = {} }
