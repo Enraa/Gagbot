b87716e219e84cf9a7085df1899ec776a73592ae
Enraa <enraaaery@gmail.com>
2026-06-17 18:36:33 -0700

    more servers

--- functions/getters/collar/getCollarTimelock.js
+++ functions/getters/collar/getCollarTimelock.js
@@ -4,18 +4,19 @@ const { getCollar } = require("./getCollar");
 /***********
  * Returns UNIX timestring of the wearer's unlock time for their collar if they are timelocked.
  * 
+ * - (server id) serverID - The server this is running on
  * - (user id) user - The User ID wearing the collar.
  * - (boolean) UNIXTimestring? - If true, returns a Discord UNIX timestring instead
  * ---
  * ##### Returns an integer with the unlockTime or a string with the unlock time for Discord.
  ***********/
-function getCollarTimelock(user, UNIXTimestring) {
+function getCollarTimelock(serverID, user, UNIXTimestring) {
     traceFirstParam(arguments[0]);
 	if (!UNIXTimestring) {
-		return getCollar(user)?.unlockTime;
+		return getCollar(serverID, user)?.unlockTime;
 	} else {
-		if (getCollar(user)?.unlockTime) {
-			return `<t:${Math.floor(getCollar(user)?.unlockTime / 1000)}:f>`;
+		if (getCollar(serverID, user)?.unlockTime) {
+			return `<t:${Math.floor(getCollar(serverID, user)?.unlockTime / 1000)}:f>`;
 		} else {
 			return null;
 		}
