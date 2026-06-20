de58896f52b25da1bd216643dbbd9250d80a9f56
EssenceOfnull <tomaskrul@gmail.com>
2026-06-20 19:44:45 +0300

    Initial ring gag update

--- gags/ring.js
+++ gags/ring.js
@@ -1,27 +1,92 @@
+/***************************
+ * Ring gag - Ring gag update
+ * ~ Kali 
+ * Standing on the shoulders of giants...
+ * Inspired by: 
+ * Ball Gag setup by DollLia
+ ***************************/
+
+// Character map stored in a separate file for code cleanliness/reusability
+const { ringGagCharMap } = require("./ring/ringCharMap.js");
+
+const isAllCaps = (text) => {
+	return text == text.toUpperCase() && /[A-Z]/g.test(text);
+};
+
+const totalAlphas = (text) => {
+	let count = 0;
+	for (let itr = 0; itr < text.length; itr++) {
+		if (text[itr].match(/[A-Za-z]/)) {
+			count++;
+		}
+	}
+	return count;
+};
+
 const garbleText = (text, parent, intensity) => {
-	let newtextparts = text.split(" ");
-	let outtext = "";
-	for (let i = 0; i < newtextparts.length; i++) {
-		for (let t = 0; t < newtextparts[i].length; t++) {
-			if (newtextparts[i].charAt(t).match(/[a-z]/i)) {
-				if (t == 0) {
-					outtext = `${outtext}g`;
-				} else {
-					outtext = `${outtext}h`;
-				}
-			} else if (newtextparts[i].charAt(t).match(/[A-Z]/i)) {
-				if (t == 0) {
-					outtext = `${outtext}G`;
-				} else {
-					outtext = `${outtext}H`;
-				}
+	let output = "";
+	//splits by regex of any whitespace char so we have word arra
+	let words = text.split(/\s/);
+
+	// custom Ring gag addition - check for word amount and if past certain threshold add a chance to interrupt
+	let doGagInterrupt = false;
+	let gagInterruptUsed = false;
+	if (words.length > 8) {
+		doGagInterrupt = true;
+	}
+
+	for (let x = 0; x < words.length; x++) {
+		/* Gag interrupt message */
+		// TODO: implement gag interrupt with user pronoun respective messages
+		
+		/* gag garble code - quick adaptation from DollLia's ball gag one */
+		let allCaps = isAllCaps(words[x]);
+		// Special case for "I", "a", etc.
+		if (allCaps && totalAlphas(words[x]) == 1) {
+			if ((words[x - 1] && isAllCaps(words[x - 1])) || (words[x + 1] && isAllCaps(words[x + 1]))) {
 			} else {
-				outtext = `${outtext}${newtextparts[i].charAt(t)}`;
+				allCaps = false;
 			}
 		}
-		outtext = `${outtext} `;
+
+		let itr = 0;
+		let prevChar = null;
+		for (const char of words[x]) {
+			// Test for uppercase.
+			let isUppercase = allCaps || char != char.toLowerCase();
+
+			// Get the new character.
+			let newChar = ringGagCharMap.get(char.toLowerCase());
+
+			if (newChar) {
+				// If char is mapped, swap it
+
+				let nextChar;
+				if (newChar.length == 2 && char.toLowerCase() == (prevChar ? prevChar.toLowerCase() : null)) {
+					//console.log("Prev: " + prevChar + "; Next: " + char)
+					nextChar = isUppercase ? newChar[1].toUpperCase() : newChar[1];
+				} else {
+					nextChar = isUppercase ? newChar[0].toUpperCase() + (newChar[1] ? newChar[1] : "") : newChar;
+				}
+
+				if (allCaps) {
+					nextChar = nextChar.toUpperCase();
+				}
+				output += nextChar;
+			} else {
+				// Append an unmodified character.
+				output += char;
+			}
+			prevChar = char; // Store previous char
+			itr++; // THEN iterate
+		}
+
+		if (x < words.length - 1) {
+			output += " ";
+		}
 	}
-	return outtext;
+
+	return output;
 };
 
 exports.garbleText = garbleText;
---
+++ gags/ring/ringCharMap.js
@@ -0,0 +1,37 @@
+/************************************************
+ * Character Map for Ring Gag
+ * 
+ * ~Kali :3
+ ************************************************/
+
+const ringCharMap = new Map([
+    ['a',"a"],
+    ['b',"b"],
+    ['c',"e"],
+    ['d',"m"],
+    ['e',"e"],
+    ['f',"h"],
+    ['g',"m"],
+    ['h',"h"],
+    ['i',"i"],
+    ['j',"a"],
+    ['k',"k"],
+    ['l',"a"],
+    ['m',"m"],
+    ['n',"n"],
+    ['o',"o"],
+    ['p',"p"],
+    ['q',"kh"],
+    ['r',"a"],
+    ['s',"z"],
+    ['t',"e"],
+    ['u',"u"],
+    ['v',"v"],
+    ['w',"w"],
+    ['x',"kh"],
+    ['y',"y"],
+    ['z',"s"],
+]);
+
+// Exporting the map for use in other functions.
+exports.ringGagCharMap = ringCharMap
