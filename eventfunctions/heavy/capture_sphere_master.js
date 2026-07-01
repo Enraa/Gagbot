34fba7f0f9472ff8488699f3ec8fd1ea46882920
NewBunnySmile <nici800900@gmail.com>
2026-07-02 00:16:09 +0200

    Removed unnecessary requires on capture_sphere_master

--- eventfunctions/heavy/capture_sphere_master.js
+++ eventfunctions/heavy/capture_sphere_master.js
@@ -1,13 +1,6 @@
 const { getProcessVariable } = require("../../functions/getters/config/getProcessVariable.js")
-const { getRecentChannel } = require("../../functions/getters/config/getRecentChannel.js")
-const { getUserVar } = require("../../functions/getters/config/getUserVar")
-const { getHeavy } = require("../../functions/getters/heavy/getHeavy")
-const { messageSendChannel } = require("../../functions/messagefunctions")
-const { setProcessVariable } = require("../../functions/setters/config/setProcessVariable.js")
 const { setUserVar } = require("../../functions/setters/config/setUserVar")
-const { removeHeavy } = require("../../functions/setters/heavy/removeHeavy")
-const { getText } = require("../../functions/textfunctions")
-const { calculatecapture } = require("./capture_sphere.js") // reuse the calculation!
+const { calculatecapture, checkUserEventsForSpheres, doSphereTick } = require("./capture_sphere.js") // reuse the calculation!
 
 let tick = async (serverID, userID, datain) => {
     checkUserEventsForSpheres(serverID, userID);
