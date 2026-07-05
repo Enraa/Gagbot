f95e08ff22f14603722eb767ccc1b2fd440c977e
Enraa <enraaaery@gmail.com>
2026-07-04 18:20:37 -0700

    Update getCurrentHoliday.js

--- functions/events/getCurrentHoliday.js
+++ functions/events/getCurrentHoliday.js
@@ -2,7 +2,7 @@
  * Outputs a current holiday, if any. Note, these use times given by the LOCAL time as known by the bot.
  * 
  * ---
- * ##### Returns the string name of a current holiday, if any. Currently supports Locktober, Christmas
+ * ##### Returns the string name of a current holiday, if any. Currently supports Locktober, Christmas, NNN
  ************/
 function getCurrentHoliday() {
     let currtime = new Date();
@@ -10,6 +10,11 @@ function getCurrentHoliday() {
     // Check for Locktober - the ENTIRE month of October
     if (currtime.getMonth() == 9) {
         return "Locktober"
+    }
+
+    // Check for NNN - the ENTIRE month of November
+    if (currtime.getMonth() == 9) {
+        return "NNN"
     }
 
     // Check for Christmas - The 25th of December
