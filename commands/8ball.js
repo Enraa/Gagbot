8603c799119a2b9983d1c76cbc3aacb37558ac53
Enraa <enraaaery@gmail.com>
2026-07-11 22:36:43 -0700

    8ball more responses

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,48 +1,6 @@
 const { SlashCommandBuilder, MessageFlags } = require("discord.js");
 const { modifymessage } = require("../functions/gagfunctions");
-
-let ballanswers = [
-    `It is certain`,
-    `It is decidedly so`,
-    `Without a doubt`,
-    `Yes definitely`,
-    `You may rely on it`,
-    `As I see it, yes`,
-    `Most likely`,
-    `Outlook good`,
-    `Yes`,
-    `Signs point to yes`,
-    `Reply hazy, try again`,
-    `Ask again later`,
-    `Better not tell you now`,
-    `Cannot predict now`,
-    `Concentrate and ask again`,
-    `Don't count on it`,
-    `My reply is no`,
-    `My sources say no`,
-    `Outlook not so good`,
-    `Very doubtful`,
-    `Perhaps if you wear these handcuffs`,
-    `Too many chains, ask later`,
-    `Muffled moans point to yes`,
-    `It is locked in!`,
-    `Only if you behave`,
-    `Maybe if you're good`,
-    `Wear a gag for me to find out`,
-    `Perhaps if I feel like it`,
-    `The future is unclear, wear a blindfold`,
-    `Bind your arms for good luck`,
-    `Almost certainly`,
-    `My reply is mmmph~!`,
-    `My sources say mmmph~!`,
-    `Maybe if you get permission`,
-    `No`,
-    `Only if I permit it`,
-    `Answer is gagged, ask again`,
-    `If you squirm while asking again, maybe`,
-    `I think not`,
-    `Ask again after a timelock`,
-]
+const { getGag8Ball } = require("../lists/8ballresponses");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -52,7 +10,11 @@ module.exports = {
 	async execute(interaction) {
 		try {
 			// We really dont need consent for 8ball responses lol
-            // Validate that the choice we received is on the premade autocompletes list!
+            let data = {
+                serverID: interaction.guildId,
+                interactionuser: interaction.user,
+                targetuser: interaction.user,
+            }
 			let questiontext = interaction.options.getString("question");
             if (questiontext && questiontext.length > 0) {
                 questiontext = await modifymessage({ content: questiontext, guildId: interaction.guildId, author: interaction.user, member: interaction.member, guild: interaction.guild }, undefined, true)
@@ -61,8 +23,8 @@ module.exports = {
             else {
                 questiontext = `${interaction.user} asks the magic Gagball for a response:\n\n`
             }
-			questiontext = `${questiontext}***${ballanswers[Math.floor(Math.random() * ballanswers.length)]}***`
-			await interaction.reply(questiontext);
+            data.c1 = questiontext;
+			await interaction.reply(`${questiontext}***${getGag8Ball(data)}***`);
 		} catch (err) {
 			console.log(err);
 		}
